8fb9375a8d7f4f0f989fd518607307ad43578f77
Mohammad Rafiqul Islam <mri@init.local>
2019-08-07 11:01:22 +0600

    update test

--- Server/ERPWebApiService/Controllers/DropdownController.cs
+++ Server/ERPWebApiService/Controllers/DropdownController.cs
@@ -35,6 +35,11 @@ namespace ERPWebApiService.Controllers
                     Value = x.Id,
                     Text = x.UnitName
                 }).ToList();
+                var unitList4 = ERPContext.Units.Select(x => new SelectListItem
+                {
+                    Value = x.Id,
+                    Text = x.UnitName
+                }).ToList();
                 return Request.CreateResponse(HttpStatusCode.OK, unitList);
             }
             catch (Exception ex)
